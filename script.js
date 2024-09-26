99e3e18c7ccf3a4ea978c219bd0d5c3c59be0b99
ricpeace <peace2make@gmail.com>
2024-09-26 17:11:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
+});
